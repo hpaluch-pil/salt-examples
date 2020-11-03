1e48a36005d67db11c6237718a73d03a51b04871
Henryk Paluch <henryk.paluch@pickering.cz>
2020-11-03 13:00:42 +0000

    Script to copy changed files to git project

---
+++ copy_changed.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+cd `dirname $0`
+
+for i in srv/*/*.sls
+do
+	src="$i"
+	[ -f "/$src" ] || {
+		echo "WARN: File '/$src' does not exist - IGNORED"
+		continue;
+	}
+
+	[ -r "/$src" ] || {
+		echo "WARN: File '/$src' unreadable - IGNORED"
+		ls -l "/$src"
+		continue;
+	}
+
+	#echo "'$src' -> '$i'"
+	diff --color=always -u "$i" "/$src" || {
+		echo -n "File $i changed - copy [y/N]?"
+		read ans
+		case "$ans" in
+			y*|Y*)
+				cp -v	"/$src" "$i" 
+				;;
+			*)
+				echo "Copy Skipped"
+				;;
+		esac
+	}
+done
